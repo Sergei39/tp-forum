e94b57c4d02e596d32a89c7afdc84a6cab06012d
Sergei Shishkin <3shishkin39@mail.ru>
2021-04-21 10:27:09 +0300

    schema tables

---
+++ tabels.sql
@@ -0,0 +1,48 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nickname TEXT,
+    fullname TEXT,
+    about TEXT,
+    email TEXT,
+    -- password BYTEA --???
+);
+
+CREATE TABLE forums (
+    id SERIAL PRIMARY KEY,
+    user_create INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    title TEXT,
+    slug TEXT, -- человекочетаемый URL
+    -- возможная оптимизация в будущем
+    -- создать поля с кол-вом сообщений и кол-вом обсуждений
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_create INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    forum INTEGER REFERENCES forums(id) ON DELETE CASCADE,
+    message TEXT, -- описание ветки
+    -- возможная оптимизация в будущем
+    -- создать поля с кол-вом голосов
+    slug TEXT,
+    created TIMESTAMP,
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    parent INTEGER DEFAULT 0 REFERENCES posts(id) ON DELETE CASCADE,
+    forum INTEGER REFERENCES forums(id) ON DELETE CASCADE,
+    author INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    thread INTEGER REFERENCES threads(id) ON DELETE CASCADE,
+    created TIMESTAMP,
+    message TEXT,
+    is_edited BOOLEAN
+);
+
+CREATE TABLE votes (
+    id SERIAL PRIMARY KEY,
+    user_create INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    thread INTEGER REFERENCES threads(id) ON DELETE CASCADE,
+    voice INTEGER
+);
