1b38c174d7d4867863f5f0d65c5143ab93479a1a
Сергей Шишкин <sergeishishkin@Sergejs-MacBook-Pro.local>
2021-06-22 00:47:43 +0300

    1598

--- tabels.sql
+++ tabels.sql
@@ -193,7 +193,8 @@ CREATE INDEX IF NOT EXISTS forum_slug ON forums (slug);
 -- CREATE INDEX IF NOT EXISTS forums_user_user ON forums_users (user_nickname); -- подумать надо ли
 CREATE INDEX IF NOT EXISTS forums_user_forum ON forums_users (forum); -- не факт что нужно после изменения схемы бд
 -- надо
-CREATE INDEX IF NOT EXISTS forums_users_forum_nickname on forums_users (forum, user_nickname); -- для получения всех юзеров из форума
+-- CREATE INDEX IF NOT EXISTS forums_users_forum_nickname on forums_users (forum, user_nickname); -- для получения всех юзеров из форума
+CREATE INDEX forum_users_all on forums_users (forum, user_nickname, user_fullname, user_about, user_email); -- тестовый
 
 -- надо
 CREATE INDEX IF NOT EXISTS user_nickname ON users using hash (nickname);
@@ -201,7 +202,7 @@ CREATE INDEX IF NOT EXISTS user_nickname ON users using hash (nickname);
 CREATE INDEX IF NOT EXISTS user_email ON users using hash (email);
 CREATE INDEX IF NOT EXISTS user_all ON users (nickname, fullname, about, email); -- тестовая
 
-CREATE INDEX IF NOT EXISTS thr_slug ON threads using hash (slug) WHERE slug != '';
+CREATE INDEX IF NOT EXISTS thr_slug ON threads using hash (slug);
 CREATE INDEX IF NOT EXISTS thr_forum ON threads using hash (forum); -- для получения всех веток из форума
 CREATE INDEX IF NOT EXISTS thr_forum_created on threads (forum, created);
 CREATE INDEX IF NOT EXISTS thr_all on threads (forum, slug, created, title, user_create, message, votes); -- тестовая
